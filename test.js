51d6af464d1eb23d65d0fb23b63c3cbf60e63284
moha hamza <hamzamm511@gmail.com>
2024-07-23 14:57:07 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+/*console.table(["moh",33,"mohito"]);
+console.log("%c ana moh ya 9iw", "color: blue; font-size: 20px;");//you have to use %c
+var a ="moh",b="didine";
+console.log(a + b);
+console.log(`moh
+didine ${a} oui ana ${b}`);*/
